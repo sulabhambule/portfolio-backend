8fc5f40b48a47454b9437e573397db439a164a1f
Sulabh <sulabhambule@gmail.com>
2025-05-03 10:25:19 +0530

    deployment done

--- src/app.js
+++ src/app.js
@@ -4,11 +4,29 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+const allowedOrigins = [
+  "https://portfolio-backend-cyan-sigma.vercel.app",
+  "http://localhost:5173",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173" || "*",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
   })
 );
+
+// app.use(
+//   cors({
+//     origin:  "http://localhost:5173" || "*",
+//   })
+// );
 
 app.use(express.json({ limit: "60mb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
